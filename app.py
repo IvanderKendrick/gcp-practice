a8fa331e5c417c3532c415921781988837ab09f5
IvanderKendrick <ken100840@gmail.com>
2026-04-25 20:37:56 +0700

    first commit

---
+++ app.py
@@ -0,0 +1,43 @@
+from fastapi import FastAPI
+import psycopg2
+import os
+
+app = FastAPI()
+
+DB_HOST = os.getenv("DB_HOST")
+DB_NAME = os.getenv("DB_NAME")
+DB_USER = os.getenv("DB_USER")
+DB_PASS = os.getenv("DB_PASS")
+
+def get_conn():
+    return psycopg2.connect(
+        host=DB_HOST,
+        database=DB_NAME,
+        user=DB_USER,
+        password=DB_PASS
+    )
+
+@app.get("/")
+def home():
+    return {"message": "API jalan 🚀"}
+
+@app.get("/users")
+def get_users():
+    conn = get_conn()
+    cur = conn.cursor()
+    cur.execute("CREATE TABLE IF NOT EXISTS users (id SERIAL PRIMARY KEY, name TEXT);")
+    cur.execute("SELECT * FROM users;")
+    rows = cur.fetchall()
+    cur.close()
+    conn.close()
+    return rows
+
+@app.post("/users")
+def add_user(name: str):
+    conn = get_conn()
+    cur = conn.cursor()
+    cur.execute("INSERT INTO users (name) VALUES (%s);", (name,))
+    conn.commit()
+    cur.close()
+    conn.close()
+    return {"status": "user ditambah"}
